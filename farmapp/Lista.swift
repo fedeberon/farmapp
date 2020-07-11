7d8020e13c78b810bb18a831e826ebdabf7e8dfe
fedeberon <Mendoza2020>
2020-07-11 14:26:03 -0300

    Calendario

---
+++ farmapp/Lista.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  farmapp
+//
+//  Created by Fede Beron on 11/07/2020.
+//  Copyright © 2020 Fede Beron. All rights reserved.
+//
+
+import Foundation
+
+struct Lista {
+    
+    var titulo: String
+    
+}
+
+
