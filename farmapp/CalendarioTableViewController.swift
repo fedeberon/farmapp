f2b4981fe1e2d73a83be4fcc4ec7988d71aedf04
fedeberon <Mendoza2020>
2020-08-01 12:54:19 -0300

    Demo Final

--- farmapp/CalendarioTableViewController.swift
+++ farmapp/CalendarioTableViewController.swift
@@ -108,11 +108,7 @@ class CalendarioTableViewController: UITableViewController {
       return cell
     }
     
-  
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        //cell.backgroundColor = UIColor.clear
-    }
-    /*
+     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
--- farmapp/SecondViewController.swift
+++ farmapp/SecondViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var ideaas: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-         self.title = "Calendario"
+        
+ ideaas.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+ ideaas.layer.shadowOffset = CGSize(width: 6.0, height: 6.0)
+ ideaas.layer.shadowOpacity = 1.0
+ ideaas.layer.shadowRadius = 2.0
+ ideaas.layer.masksToBounds = false
+ ideaas.layer.cornerRadius = 4.0
     }
 
 
